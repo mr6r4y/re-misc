a9b668967033645d73b4b09b0140eee3378a060d
mr6r4y <mr6r4y@gmail.com>
2017-06-28 12:07:45 +0300

    Fix

--- dump_funcs.py
+++ dump_funcs.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 import r2pipe as r2p
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Dump dissassembly of all analysed funcs")
-    parser.add_argument("-f", "--file",
-                        help="Path to file for analysis")
+    parser.add_argument("-d", "--directory", default="./",
+                        help="Path to directory to dump files")
 
     args = parser.parse_args()
 
     return args
 
 
-def dump_funcs(fl):
+def dump_funcs(directory):
     r = r2p.open()
-    with open(fl, "w") as f:
-        for i in r.cmdj("aflj"):
-            f.write("0x%x : %s\n\n" % (i["offset"], i["name"]))
-            r.cmd("s 0x%x" % i["offset"])
-            f.write(r.cmd("pdf"))
-            f.write("\n\n\n")
+    for i in r.cmdj("aflj"):
+        fl = os.path.join(directory, i["name"])
+        r.cmd("pdf @ 0x%x > %s.asm" % (i["offset"], fl))
 
 
 def main():
     args = get_args()
-    fl = args.file
+    dr = os.path.abspath(args.directory)
 
-    dump_funcs(fl)
+    dump_funcs(dr)
 
 
 if __name__ == "__main__":
